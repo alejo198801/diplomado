e564c9755f28c10e4711e29f9183a9291da76e1d
yamith <alcastano001@gmail>
2022-11-24 23:30:05 -0500

    seguridad.

---
+++ Springboot_backend/src/main/java/seguridad/TokenUtils.java
@@ -0,0 +1,10 @@
+package seguridad;
+
+
+public class TokenUtils {
+	
+	private final static String ACCESS_TOKEN_SECRET="Tnmdsnda45542a15694er";
+	private final static Long ACESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
+	
+
+}
